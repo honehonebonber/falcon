809390da16da13ae85bc00e18e9d4a59652af158
yuito imamura <yui7.mario@gmail.com>
2017-07-14 19:36:07 +0900

    no message

--- Assets/CommandButtonController.cs
+++ Assets/CommandButtonController.cs
@@ -5,11 +5,13 @@ using UnityEngine.UI;
 public class CommandButtonController : MonoBehaviour {
 
 	public Const.Commnad command;
+	public GameObject Icon;
 
 	private void Start () {
 		var button = GetComponent<Button>();
 		button.onClick.AddListener (() => {
 			BattleUIManager.instance.PushButton(this.command);
+			Instantiate(Icon);
 		});
 
 	}
